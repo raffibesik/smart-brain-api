6e315a2e41a76f40ce65de40b1792a18462532e0
Raffi Besiktasliyan <40692018+Raffi-Besik@users.noreply.github.com>
2019-11-04 14:27:05 +0100

    First commit - added controllers and API's

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const knex = require('knex');
+
+const register = require('./controller/register');
+const signIn = require('./controller/signIn');
+const profile = require('./controller/profile');
+const image = require('./controller/image');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        user: '',
+        password: '',
+        database: 'smart-brain'
+    }
+});
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/profile/:id', profile.handleProfileGet(db));
+app.post('/signin',  signIn.handleSignIn(db, bcrypt));
+app.post('/register', register.handleRegister(db, bcrypt, saltRounds));
+app.put('/image', image.handleImage(db));
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)} );
+
+app.listen(3000 || process.env.PORT, () => {
+    console.log(`app is running on port ${3000 || process.env.PORT}`);
+});
+
