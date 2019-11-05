57f6fee15a9e96c5be1108a31d5bc5809a785dbc
Raffi Besiktasliyan <raffi.besik@hotmail.com>
2019-11-05 12:12:51 +0100

    added database env

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ const image = require('./controller/image');
 const db = knex({
     client: 'pg',
     connection: {
-        host: 'postgresql-convex-72732',
-        user: '',
-        password: '',
-        database: 'smart-brain'
+        host: process.env.DATABASE_URL,
+        ssl: true
     }
 });
 
