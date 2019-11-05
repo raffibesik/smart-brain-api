d9d03c9a49068cfdc51b04a220e4625ffd51a898
Raffi Besiktasliyan <raffi.besik@hotmail.com>
2019-11-05 12:09:44 +0100

    updated database

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const image = require('./controller/image');
 const db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
+        host: 'postgresql-convex-72732',
         user: '',
         password: '',
         database: 'smart-brain'
